180313c8f52c71903a6fa02efdc0d077b9c27fbd
nandakishore01 <nandakishoreg123@gmail.com>
2020-06-14 23:19:08 +0530

    5th message

--- src/main/java/com/app/SpringFoxConfig.java
+++ src/main/java/com/app/SpringFoxConfig.java
@@ -13,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SpringFoxConfig {                                    
     
+	//For Creating Docket Bean
     @Bean
     public Docket api() {                
         return new Docket(DocumentationType.SWAGGER_2)
