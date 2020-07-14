756f0bf6a5ededad92fb13b8aecac88fc888b563
nandakishore01 <nandakishoreg123@gmail.com>
2020-07-14 16:43:58 +0530

    commit 11 message

--- src/main/java/com/app/BootGradleApplication.java
+++ src/main/java/com/app/BootGradleApplication.java
@@ -10,6 +10,7 @@ public class BootGradleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootGradleApplication.class, args);
-		System.out.println("============12345678910============");
+		System.out.println("============1234567891011============");
+		System.out.println("=============11 commit======");
 	}
 }
